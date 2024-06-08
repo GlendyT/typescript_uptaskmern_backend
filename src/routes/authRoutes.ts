b7c3f7f188f7bfc82a65a9084b9a82827dc8c9eb
GlendyT <glendytuyuc@gmail.com>
2024-06-08 15:58:36 -0600

    Registro de usuarios, creando el controllador y validacion, hasear los passwords con bcrypt y prevenir usuarios duplicados

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,23 @@
 import { Router } from "express";
+import { AuthController } from "../controllers/AuthController";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middleware/validation";
 
-const router = Router()
+const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("desde /api/auth")
-})
+router.post(
+  "/create-account",
+  body("name").notEmpty().withMessage("El mensaje no puede ir bacio"),
+  body("password").isLength({min: 8}).withMessage("El password es muy corto, minimo 8 caracteres"),
+  body("password_confirmation").custom((value, {req}) => {
+    if(value !== req.body.password) {
+        throw new Error ("Los Password no son iguales")
+    }
+    return true
+  }),
+  body("email").isEmail().withMessage("E-main no valido"),
+  handleInputErrors,
+  AuthController.createAccount
+);
 
-export default router
+export default router;
